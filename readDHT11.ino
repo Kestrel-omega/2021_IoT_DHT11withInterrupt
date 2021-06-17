56d0005e8564e8b51d236855a0cf2bf5552a6ff6
Kestrel_omega <40593588+Kestrel-omega@users.noreply.github.com>
2021-06-18 04:04:17 +0900

    GPIO 인터럽트 구현 완료

--- readDHT11.ino
+++ readDHT11.ino
@@ -1,10 +1,66 @@
 #include <Arduino.h>
-#define DHT11PIN 14 //D4
+#define DHT11PIN 2 //D4
 
-int readDHT11(int *readTemp, int* readHumid)
+int microTime[42];
+int dt[40];
+int cnt;
+int flag;
+
+void timeoutHandler()
 {
+  cnt++;
+  if(cnt >=2500)
+  {
+    cnt = 0;
+  }
+}
 
-  int dt[82] = {0,};
+ICACHE_RAM_ATTR void pulseHandler()
+{
+  cnt++;
+  microTime[cnt] = micros();
+}
+
+int readDHTInterrupt(int *readTemp, int* readHumid)
+{
+  detachInterrupt(digitalPinToInterrupt(DHT11PIN));
+  microTime[42] = {0,};
+  cnt = 0;
+  digitalWrite(DHT11PIN, 1);
+  pinMode(DHT11PIN, OUTPUT);
+  delay(1);
+  digitalWrite(DHT11PIN, 0);
+  delay(20);
+  pinMode(DHT11PIN, INPUT_PULLUP);
+  attachInterrupt(digitalPinToInterrupt(DHT11PIN), pulseHandler, FALLING);
+
+  for(int i=2; i<42; i++)
+    dt[i] = microTime[i]-microTime[i-1];
+
+  *readHumid = 0;
+  *readTemp = 0;
+  for (int j = 2; j < 11; j++) //Humid
+  {
+    *readHumid = *readHumid << 1;
+    if (dt[j] > 100)
+      *readHumid = *readHumid + 1;
+    else
+      *readHumid = *readHumid + 0;
+  }
+  for (int k = 18; k < 26; k++) //Temp
+  {
+    *readTemp = *readTemp << 1;
+    if (dt[k] > 100)
+      *readTemp = *readTemp + 1;
+    else
+      *readTemp = *readTemp + 0;
+  }
+  return 1;
+}
+
+int readDHT11Polling(int *readTemp, int* readHumid)
+{
+  dt[82] = {0,};
   //Phase 1
   digitalWrite(DHT11PIN, 1);
   pinMode(DHT11PIN, OUTPUT);
@@ -12,12 +68,7 @@ int readDHT11(int *readTemp, int* readHumid)
   digitalWrite(DHT11PIN, 0);
   delay(20);
   pinMode(DHT11PIN, INPUT_PULLUP);
-  /*
-    while(1)
-      if(digitalRead(DHT11PIN)==1) break;
-    while(1)
-      if(digitalRead(DHT11PIN)==0) break;
-  */
+  
   //Phase 2, 3
   int cnt = 0;
   for (cnt = 0; cnt < 41; cnt++)
@@ -66,7 +117,12 @@ void setup()
 
 void loop()
 {
+  cnt = 0;
+  dt[cnt] = micros();
   int readTemp, readHumid;
-  readDHT11(&readTemp, &readHumid);
-  delay(1000);
+  readDHTInterrupt(&readTemp, &readHumid);
+  
+  Serial.printf("Temperature : %d, Humidity : %d\r\n", readTemp, readHumid);
+  
+  delay(5000);
 }
